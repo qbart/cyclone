7143c57f04d8218907c832cf6cc9d69ccb95f377
qbart <wojtowicz.bartlomiej@gmail.com>
2020-06-22 02:36:00 +0200

    Add some list operations

--- cyclone/list.go
+++ cyclone/list.go
@@ -1,14 +1,210 @@
 package cyclone
 
-import "github.com/mediocregopher/radix/v3"
+import (
+	"strconv"
+
+	"github.com/mediocregopher/radix/v3"
+)
 
 type List struct {
 	cyclone *Cyclone
 	key     string
 }
 
-func (l *List) Push(elem string) int {
-	var count int
-	l.cyclone.Raw.Do(radix.Cmd(&count, "LPUSH", l.key, elem))
-	return count
+//TODO: BLPOP
+//TODO: BRPOP
+//TODO: BRPOPLPUSH
+
+// Index (LINDEX) Returns the element at index index in the list stored at key.
+// The index is zero-based, so 0 means the first element, 1 the second element
+// and so on. Negative indices can be used to designate elements starting at the
+// tail of the list. Here, -1 means the last element, -2 means the penultimate
+// and so forth.
+// https://redis.io/commands/lindex
+//
+// Time complexity: O(N) where N is the number of elements to traverse to get to the
+//                  element at index. This makes asking for the first or the last
+//                  element of the list O(1).
+func (l *List) Index(index int) (elem string) {
+	l.cyclone.Raw.Do(radix.Cmd(&elem, "LINDEX", l.key, strconv.Itoa(index)))
+	return
+}
+
+//TODO: LINSERT
+
+// Len returns the length of the list stored at key. If key does not exist,
+// it is interpreted as an empty list and 0 is returned.
+// https://redis.io/commands/llen
+//
+// Time complexity: O(1)
+func (l *List) Len() (lenOfList int) {
+	l.cyclone.Raw.Do(radix.Cmd(&lenOfList, "LLEN", l.key))
+	return
+}
+
+// Pop (LPOP) Removes and returns the first element of the list stored at key.
+// https://redis.io/commands/lpop
+//
+// Time complexity: O(1)
+func (l *List) Pop() (elem string) {
+	l.cyclone.Raw.Do(radix.Cmd(&elem, "LPOP", l.key))
+	return
+}
+
+//TODO: LPOS
+
+// Push (LPUSH) Inserts all the specified values at the head of the list stored at key.
+// If key does not exist, it is created as empty list before performing the push
+// operations. When key holds a value that is not a list, an error is returned.
+// https://redis.io/commands/lpush
+//
+// Time complexity: O(1) for each element added, so O(N) to add N
+//                  elements when the command is called with multiple arguments.
+func (l *List) Push(elems ...interface{}) (lenAfterPush int) {
+	l.cyclone.Raw.Do(radix.FlatCmd(
+		&lenAfterPush,
+		"LPUSH",
+		l.key,
+		elems...,
+	))
+	return
+}
+
+// PushX (LPUSHX) Inserts specified values at the head of the list stored at key,
+// only if key already exists and holds a list. In contrary to LPUSH, no operation
+// will be performed when key does not yet exist.
+// https://redis.io/commands/lpushx
+//
+// Time complexity: O(1) for each element added, so O(N) to add N elements
+//                  when the command is called with multiple arguments.
+func (l *List) PushX(elems ...interface{}) (lenAfterPush int) {
+	l.cyclone.Raw.Do(radix.FlatCmd(
+		&lenAfterPush,
+		"LPUSHX",
+		l.key,
+		elems...,
+	))
+	return
+}
+
+// Range (LRANGE) Returns the specified elements of the list stored at key.
+// The offsets start and stop are zero-based indexes, with 0 being the first
+// element of the list (the head of the list), 1 being the next element and so on.
+//
+// These offsets can also be negative numbers indicating offsets starting at the
+// end of the list. For example, -1 is the last element of the list, -2 the
+// penultimate, and so on.
+//
+// https://redis.io/commands/lrange
+//
+// Time complexity: O(S+N) where S is the distance of start offset from HEAD for small
+//                  lists, from nearest end (HEAD or TAIL) for large lists; and N is
+//                  the number of elements in the specified range.
+func (l *List) Range(start, stop int) (elems []string) {
+	l.cyclone.Raw.Do(radix.Cmd(
+		&elems,
+		"LRANGE",
+		l.key,
+		strconv.Itoa(start),
+		strconv.Itoa(stop),
+	))
+	return
+}
+
+// Rem (LREM) Removes the first count occurrences of elements equal to element
+// from the list stored at key. The count argument influences the operation
+// in the following ways:
+// - count > 0: Remove elements equal to element moving from head to tail.
+// - count < 0: Remove elements equal to element moving from tail to head.
+// - count = 0: Remove all elements equal to element.
+// https://redis.io/commands/lrem
+//
+// Time complexity: O(N+M) where N is the length of the list and M is the
+//                  number of elements removed.
+func (l *List) Rem(count int, elem string) (removedElems int) {
+	l.cyclone.Raw.Do(radix.Cmd(
+		&removedElems,
+		"LREM",
+		l.key,
+		strconv.Itoa(count),
+		elem,
+	))
+	return
+}
+
+// Set sets the list element at index to element.
+// https://redis.io/commands/lset
+//
+// Time complexity: O(N) where N is the length of the list. Setting either
+//                  the first or the last element of the list is O(1).
+func (l *List) Set(index int, elem string) bool {
+	err := l.cyclone.Raw.Do(radix.Cmd(
+		nil,
+		"LSET",
+		l.key,
+		strconv.Itoa(index),
+		elem,
+	))
+	return err == nil
+}
+
+// Trim (LTRIM) Trim an existing list so that it will contain only the specified
+// range of elements specified. Both start and stop are zero-based indexes,
+// where 0 is the first element of the list (the head), 1 the next element and so on.
+// https://redis.io/commands/ltrim
+//
+// Time complexity: O(N) where N is the number of elements to be removed by the operation.
+func (l *List) Trim(start, stop int) bool {
+	err := l.cyclone.Raw.Do(radix.Cmd(
+		nil,
+		"LTRIM",
+		l.key,
+		strconv.Itoa(start),
+		strconv.Itoa(stop),
+	))
+	return err == nil
+}
+
+// RPop removes and returns the last element of the list stored at key.
+// https://redis.io/commands/rpop
+//
+// Time complexity: O(1)
+func (l *List) RPop() (elem string) {
+	l.cyclone.Raw.Do(radix.Cmd(&elem, "RPOP", l.key))
+	return
+}
+
+//TODO: RPOPLPUSH
+
+// RPush inserts all the specified values at the tail of the list stored at key.
+// If key does not exist, it is created as empty list before performing the push operation.
+// https://redis.io/commands/rpush
+//
+// Time complexity: O(1) for each element added, so O(N) to add N elements when
+//                  the command is called with multiple arguments.
+func (l *List) RPush(elems ...interface{}) (lenAfterPush int) {
+	l.cyclone.Raw.Do(radix.FlatCmd(
+		&lenAfterPush,
+		"RPUSH",
+		l.key,
+		elems...,
+	))
+	return
+}
+
+// RPushX inserts specified values at the tail of the list stored at key, only
+// if key already exists and holds a list. In contrary to RPUSH, no operation
+// will be performed when key does not yet exist.
+// https://redis.io/commands/rpushx
+//
+// Time complexity: O(1) for each element added, so O(N) to add N elements
+//                  when the command is called with multiple arguments.
+func (l *List) RPushX(elems ...interface{}) (lenAfterPush int) {
+	l.cyclone.Raw.Do(radix.FlatCmd(
+		&lenAfterPush,
+		"RPUSHX",
+		l.key,
+		elems...,
+	))
+	return
 }
---
+++ cyclone/list_test.go
@@ -0,0 +1,278 @@
+package cyclone
+
+import (
+	"testing"
+
+	. "github.com/franela/goblin"
+	"github.com/mediocregopher/radix/v3"
+)
+
+func TestList(t *testing.T) {
+	g := Goblin(t)
+	withConn(func(c *Cyclone) {
+		g.Describe(".BPop", func() {
+			g.Xit("", func() {
+			})
+		})
+
+		g.Describe(".BRPop", func() {
+			g.Xit("", func() {
+			})
+		})
+
+		g.Describe(".BRPopLPush", func() {
+			g.Xit("", func() {
+			})
+		})
+
+		g.Describe(".Index", func() {
+			g.It("Returns element at index", func() {
+				list := c.List("ListLIndex")
+
+				// empty list
+				elem := list.Index(0)
+				g.Assert(elem).Eql("")
+
+				// with elments
+				g.Assert(list.Push("a", "b")).Eql(2)
+				elem = list.Index(1)
+				g.Assert(elem).Eql("a")
+			})
+		})
+
+		g.Describe(".Insert", func() {
+			g.Xit("", func() {
+			})
+		})
+
+		g.Describe(".Len", func() {
+			g.It("Returns len of the list", func() {
+				c.List("ListLen").Push("a", "b", "c")
+
+				length := c.List("ListLen").Len()
+				g.Assert(length).Eql(3)
+			})
+		})
+
+		g.Describe(".Pop", func() {
+			g.It("Pops element from HEAD", func() {
+				list := c.List("ListLPop")
+
+				// empty list
+				elem := list.Pop()
+				g.Assert(elem).Eql("")
+
+				list.Push("a", "b")
+				elem = list.Pop()
+				g.Assert(elem).Eql("b")
+				g.Assert(list.Range(0, -1)).Eql([]string{"a"})
+			})
+		})
+
+		g.Describe(".Pos", func() {
+			g.Xit("", func() {
+			})
+		})
+
+		g.Describe(".Push", func() {
+			g.It("Pushes elements into list HEAD", func() {
+				c.List("ListLPush").Push("a", "b", "c")
+				length := c.List("ListLPush").Push("d")
+
+				g.Assert(length).Eql(4)
+
+				elems := c.List("ListLPush").Range(0, -1)
+				g.Assert(elems[0]).Eql("d")
+				g.Assert(elems[1]).Eql("c")
+				g.Assert(elems[2]).Eql("b")
+				g.Assert(elems[3]).Eql("a")
+			})
+		})
+
+		g.Describe(".PushX", func() {
+			g.It("Pushes elements into list HEAD only if list exists", func() {
+				c.List("ListLPushX").Push("a")
+				lenExisting := c.List("ListLPushX").PushX("b")
+				lenNonExisting := c.List("ListLPushXother").PushX("c")
+
+				g.Assert(lenExisting).Eql(2)
+				g.Assert(lenNonExisting).Eql(0)
+
+				elems := c.List("ListLPushX").Range(0, -1)
+				g.Assert(elems[0]).Eql("b")
+				g.Assert(elems[1]).Eql("a")
+
+				elems = c.List("ListLPushXother").Range(0, -1)
+				g.Assert(len(elems)).Eql(0)
+			})
+		})
+
+		g.Describe(".Range", func() {
+			g.It("Returns range from given offsets", func() {
+				c.Raw.Do(radix.Cmd(nil, "RPUSH", "ListLRange", "a", "b", "c"))
+
+				// full range
+				elems := c.List("ListLRange").Range(0, -1)
+
+				g.Assert(len(elems)).Equal(3)
+				g.Assert(elems[0]).Eql("a")
+				g.Assert(elems[1]).Eql("b")
+				g.Assert(elems[2]).Eql("c")
+
+				// single element
+				elems = c.List("ListLRange").Range(1, 1)
+
+				g.Assert(len(elems)).Equal(1)
+				g.Assert(elems[0]).Eql("b")
+
+				// out of bounds
+				elems = c.List("ListLRange").Range(4, 6)
+
+				g.Assert(len(elems)).Equal(0)
+			})
+		})
+
+		g.Describe(".Rem", func() {
+			g.It("Removes elments from list", func() {
+				list := c.List("ListLRem")
+
+				// empty list
+				g.Assert(
+					list.Rem(0, "a"),
+				).Eql(0)
+
+				// count == 0
+				list.Push("a", "b", "a", "b")
+				g.Assert(
+					list.Rem(0, "a"),
+				).Eql(2)
+				g.Assert(
+					list.Range(0, -1),
+				).Eql([]string{"b", "b"})
+
+				// count > 0
+				list.Push("c", "b", "b")
+				g.Assert(
+					list.Range(0, -1),
+				).Eql([]string{"b", "b", "c", "b", "b"})
+				g.Assert(
+					list.Rem(2, "b"),
+				).Eql(2)
+				g.Assert(
+					list.Range(0, -1),
+				).Eql([]string{"c", "b", "b"})
+
+				// count < 0
+				list.Push("b")
+				g.Assert(
+					list.Range(0, -1),
+				).Eql([]string{"b", "c", "b", "b"})
+				g.Assert(
+					list.Rem(-2, "b"),
+				).Eql(2)
+				g.Assert(
+					list.Range(0, -1),
+				).Eql([]string{"b", "c"})
+			})
+		})
+
+		g.Describe(".Set", func() {
+			g.It("Sets element at index", func() {
+				list := c.List("ListLSet")
+
+				// empty list
+				g.Assert(
+					list.Set(0, "a"),
+				).Eql(false)
+
+				// add some data
+				list.Push("b", "a")
+				g.Assert(
+					list.Range(0, -1),
+				).Eql([]string{"a", "b"})
+
+				// with exising index
+				g.Assert(
+					list.Set(1, "c"),
+				).Eql(true)
+				g.Assert(
+					list.Range(0, -1),
+				).Eql([]string{"a", "c"})
+			})
+		})
+
+		g.Describe(".Trim", func() {
+			g.It("Trims list to given range", func() {
+				list := c.List("ListLTrim")
+				list.Push("f", "e", "d", "c", "b", "a")
+
+				g.Assert(
+					list.Trim(0, 3),
+				).Eql(true)
+				g.Assert(
+					list.Range(0, -1),
+				).Eql([]string{"a", "b", "c", "d"})
+
+				g.Assert(
+					list.Trim(1, -3),
+				).Eql(true)
+				g.Assert(
+					list.Range(0, -1),
+				).Eql([]string{"b"})
+			})
+		})
+
+		g.Describe(".RPop", func() {
+			g.It("Pops element from TAIL", func() {
+				list := c.List("ListRPop")
+
+				// empty list
+				elem := list.RPop()
+				g.Assert(elem).Eql("")
+
+				list.RPush("a", "b")
+				elem = list.RPop()
+				g.Assert(elem).Eql("b")
+				g.Assert(list.Range(0, -1)).Eql([]string{"a"})
+			})
+		})
+
+		g.Describe(".RPopLPush", func() {
+			g.Xit("", func() {
+			})
+		})
+
+		g.Describe(".RPush", func() {
+			g.It("Pushes elements into list TAIL", func() {
+				c.List("ListRPush").Push("a", "b", "c")
+				length := c.List("ListRPush").RPush("d")
+
+				g.Assert(length).Eql(4)
+
+				elems := c.List("ListRPush").Range(0, -1)
+				g.Assert(elems[0]).Eql("c")
+				g.Assert(elems[1]).Eql("b")
+				g.Assert(elems[2]).Eql("a")
+				g.Assert(elems[3]).Eql("d")
+			})
+		})
+
+		g.Describe(".RPushX", func() {
+			g.It("Pushes elements into list TAIL only if list exists", func() {
+				c.List("ListRPushX").RPush("a")
+				lenExisting := c.List("ListRPushX").RPushX("b")
+				lenNonExisting := c.List("ListRPushXother").RPushX("c")
+
+				g.Assert(lenExisting).Eql(2)
+				g.Assert(lenNonExisting).Eql(0)
+
+				elems := c.List("ListRPushX").Range(0, -1)
+				g.Assert(elems[0]).Eql("a")
+				g.Assert(elems[1]).Eql("b")
+
+				elems = c.List("ListRPushXother").Range(0, -1)
+				g.Assert(len(elems)).Eql(0)
+			})
+		})
+	})
+}
